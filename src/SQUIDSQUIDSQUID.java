3890cb4dced24b91d76f2a037ca78859774731a5
vareye <vareye@mcmaster.ca>
2017-03-03 01:08:29 -0500

    This is the worst code ive ever written. I swear I did it on purpose

---
+++ src/SQUIDSQUIDSQUID.java
@@ -0,0 +1,117 @@
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+
+public class SQUIDSQUIDSQUID {
+
+
+	public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
+		
+		if(Desktop.isDesktopSupported())
+		{
+			boolean dog = true;
+			while(dog){
+			Desktop.getDesktop().browse(new URI("https://www.youtube.com/watch?v=r-5KzHDPCTM"));
+			Thread.sleep(15000);
+			Desktop.getDesktop().browse(new URI("http://www.squid-world.com/"));
+			Thread.sleep(5000);
+			System.out.println("ARE YOU READY FOR THE SQUID!!!!!!!!!!!!");
+			Desktop.getDesktop().browse(new URI("https://www.pointerpointer.com"));
+			Thread.sleep(5000);
+			Desktop.getDesktop().browse(new URI("https://c1.staticflickr.com/9/8124/8711315468_5051021517_b.jpg"));
+			Thread.sleep(5000);
+			Desktop.getDesktop().browse(new URI("https://i.ytimg.com/vi/GONddG89Go4/hqdefault.jpg"));
+			Thread.sleep(3000);
+			Desktop.getDesktop().browse(new URI("http://www.staggeringbeauty.com/"));
+			Thread.sleep(5000);
+			Desktop.getDesktop().browse(new URI("https://img.buzzfeed.com/buzzfeed-static/static/enhanced/web05/2012/6/18/16/enhanced-buzz-19596-1340052634-4.jpg"));
+			Thread.sleep(5000);
+			Desktop.getDesktop().browse(new URI("http://www.whitepowermilk.com/"));
+			Thread.sleep(5000);
+			Desktop.getDesktop().browse(new URI("http://html5zombo.com/"));
+			Thread.sleep(5000);
+			Desktop.getDesktop().browse(new URI("http://ducksarethebest.com/"));
+			Thread.sleep(5000);
+			Desktop.getDesktop().browse(new URI("http://www.sanger.dk/"));
+			Thread.sleep(5000);
+			Desktop.getDesktop().browse(new URI("http://eelslap.com/"));
+			Thread.sleep(5000);
+			Desktop.getDesktop().browse(new URI("http://beesbeesbees.com/"));
+			Thread.sleep(5000);
+			Desktop.getDesktop().browse(new URI("http://cat-bounce.com/"));
+			Thread.sleep(5000);
+			Desktop.getDesktop().browse(new URI("http://ninjaflex.com/"));
+			Thread.sleep(5000);
+			Desktop.getDesktop().browse(new URI("http://chrismckenzie.com/"));
+			Thread.sleep(5000);
+			Desktop.getDesktop().browse(new URI("http://salmonofcapistrano.com/"));
+			Thread.sleep(5000);
+			Desktop.getDesktop().browse(new URI("http://www.ouaismaisbon.ch/"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("http://crouton.net/"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("http://corgiorgy.com/"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("http://www.patience-is-a-virtue.org/"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("http://intotime.com/"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("http://leekspin.com/"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("http://tomsdog.com/"));
+			Thread.sleep(7000);
+			Desktop.getDesktop().browse(new URI("http://hardcoreprawnlawn.com/"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("http://oct82.com/"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("http://dogs.are.the.most.moe/"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("http://secretsfornicotine.com/"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("http://whitetrash.nl/"));
+			Thread.sleep(7000);
+			Desktop.getDesktop().browse(new URI("http://www.biglongnow.com/"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("http://i.imgur.com/M8XxiAD.jpg"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("http://i.imgur.com/MI93A9k.jpg"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("http://i.imgur.com/Etya64b.gif"));
+			Thread.sleep(10000);
+			Desktop.getDesktop().browse(new URI("https://pbs.twimg.com/profile_images/454851708099624960/wT7TEFFC.jpeg"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("http://www.cavemancircus.com/wp-content/uploads/images/2013/april/welcome_to_the_internet/welcome_to_the_internet_2.jpg"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("http://thetango.net/wp-content/uploads/2015/11/TheTango-WeirdWednesday-11242015-18-620x578.jpg"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("http://i.imgur.com/zkI9dgn.gif"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("http://img.ifcdn.com/images/9ad5fa1cf5e7ae2b68c99c8d5a263ab5962411d3603c95f41588845c932b0aa0_1.gif"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("http://i.imgur.com/Y8Ri1Sb.jpg"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("https://res.cloudinary.com/zergid-com/image/upload/v1/public/uploads/media/mq8zs5osdugtuih.jpg"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("http://cdn.theatlantic.com/assets/media/img/posts/2014/05/shrekgif/b1090a2ca.gif"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("https://2.bp.blogspot.com/-vuQAhd5ZboA/VVkZ6PeKlLI/AAAAAAAAtRk/X5h_E_MYtV8/s1600/welcome-to-the-weird-side-of-the-internet-81543.jpg"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("https://4.bp.blogspot.com/-2FR4DZrFaZA/VVkZyx3KUTI/AAAAAAAAtQo/a98sx0SlJ5c/s640/welcome-to-the-weird-side-of-the-internet-17228.jpg"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("http://cdn.someecards.com/posts/giphy-4-JBq3Wv.gif"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("http://i.somethingawful.com/u/garbageday/2012/phriday/contentaware/Everdraed_02.gif"));
+			Thread.sleep(6000);
+			Desktop.getDesktop().browse(new URI("http://i.imgur.com/Y8Ri1Sb.jpg"));
+			Thread.sleep(200000);
+			Desktop.getDesktop().browse(new URI("http://www.wwwdotcom.com/"));
+			dog = false;
+			}
+		  
+		}
+
+	}
+
+}
